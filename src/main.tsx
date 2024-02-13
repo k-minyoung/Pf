92bd1a080cf97fe3318d306609fc20c6eb2ce14a
k-minyoung <alsdudsk12@naver.com>
2024-02-13 18:52:42 +0900

    [design] 전체적인 틀

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import styles from "./main.module.css"
 export default function Main() {
     return (
         <div className={styles.body}>
-            <div className={styles.top}>t</div>
+            <div className={styles.top}>포트폴리오입니다</div>
+            <div className={styles.header}>
+                <div>skill</div>
+                <div>project</div>
+                <div>footer</div>
+            </div>
             <div className={styles.mid}>
+                <div className={styles.skill}>skill
+                </div>
+
+                <div className={styles.project}>project</div>
                 <div className={styles.pj1}>box1</div>
                 <div className={styles.pj2}>box2</div>
                 <div className={styles.pj3}>box3</div>
