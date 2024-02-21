64d33e51f091e3a74b95b0bb0a46c47c46447f86
k-minyoung <alsdudsk12@naver.com>
2024-02-21 20:15:57 +0900

    [Feat] scrollY  => Intersection Observer 기능 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from "./css/main.module.css";
 import Modal1 from './modal/modal1';
 import Modal2 from './modal/modal2';
@@ -10,27 +10,52 @@ export default function Main() {
 
     const [showModal, setShowModal] = useState<boolean>(false);
     const [showModal2, setShowModal2] = useState<boolean>(false);
-    const [scrollPosition, setScrollPosition] = useState<number>(0);
+    const [visible, setVisible] = useState<boolean>(false);
+
 
     const [back, setBack] = useState<boolean>(false);
 
-    //스크롤 위치 구함
-    const onScroll = () => {
-        setScrollPosition(window.scrollY)
-    }
+    //화면에 나타남을 감지
+    const io = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry, i) => {
+                if (entry.isIntersecting) {
+                    console.log(`헤더가 뷰포트에 들어옴`);
+                    setVisible(true)
 
-    //이벤트 리스너 등록
+                } else {
+                    console.log(`헤더가 뷰포트에서 나감`);
+                    setVisible(false)
+
+                }
+            });
+        },
+        {
+            root: null,
+            rootMargin: "0px",
+            threshold: 1
+            //비율 (1이면 전체가 나타나야 인정)
+        }
+    );
+
     useEffect(() => {
-        window.addEventListener("scroll", onScroll);
-        return () => {
-            window.removeEventListener("scroll", onScroll);
-        };
+        io.observe(document.querySelector("#header") as HTMLElement);//여기서 타겟 설정
     }, []);
 
+    //https://ww8007-learn.tistory.com/6 참고했음
+
+    // // 스크롤 이벤트 리스너 등록
+    // useEffect(() => {
+    //     window.addEventListener("scroll", onScroll);
+    //     return () => {
+    //         window.removeEventListener("scroll", onScroll);
+    //     };
+    // }, []);
+
     //위치 콘솔 찍어주기
-    useEffect(() => {
-        console.log(scrollPosition)
-    }, [scrollPosition])
+    // useEffect(() => {
+    //     console.log(scrollPosition)
+    // }, [scrollPosition])
 
     //모달 열기
     const show = () => {
@@ -47,10 +72,11 @@ export default function Main() {
 
 
     return (
-        <div className={styles.body}>
+        <div className={styles.body} id='body'>
             <div className={styles.top}>포트폴리오입니다</div>
-            <div className={scrollPosition >= 850 ? styles.header2 : styles.header}>
-                {/* 스크롤 위치가 850을 넘으면 header2스타일, 아니면 header스타일 적용 */}
+            {/* <div className={scrollPosition >= 850 ? styles.header2 : styles.header}> */}
+            {/* 스크롤 위치가 850을 넘으면 header2스타일, 아니면 header스타일 적용 */}
+            <div className={visible ? styles.header : styles.header2} id='header'>
                 <div className={styles.skill2}>skill</div>
                 <div className={styles.project}>project</div>
                 <div className={styles.footer}>footer</div>
